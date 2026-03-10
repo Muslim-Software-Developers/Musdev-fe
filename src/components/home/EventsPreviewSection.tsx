a397af60498324d19cac3ef03738438b5591f8ab
Yusuf Aweda <yusuf.aweda@inq.inc>
2026-03-10 04:48:09 +0100

    updated Images loading fixed edited

--- src/components/home/EventsPreviewSection.tsx
+++ src/components/home/EventsPreviewSection.tsx
@@ -13,7 +13,7 @@ const events = [
     time: "11:00 AM WAT",
     location: "JJT Park, Ikeja, Lagos",
     category: "Community",
-    image: "https://musdev.org/images/prp.jpg", // Direct path to publichttps://musdev.org/images/prp.jpg
+    image: "./images/prp.jpg", // Direct path to public/images/prp.jpg
   },
   {
     id: "musdev-annual-summit-2026",
@@ -23,7 +23,7 @@ const events = [
     time: "9:00 AM WAT",
     location: "Eko Hotel, Lagos",
     category: "Conference",
-    image: "https://musdev.org/images/mats.jpg", // Direct path to publichttps://musdev.org/images/mats.jpg
+    image: "/images/mats.jpg", // Direct path to public/images/mats.jpg
   },
   {
     id: "muslim-devs-hackathon-2026",
@@ -33,7 +33,7 @@ const events = [
     time: "8:00 AM WAT",
     location: "CcHub, Yaba, Lagos",
     category: "Hackathon",
-    image: "https://musdev.org/images/mdh.jpg", // Direct path to publichttps://musdev.org/images/mdh.jpg
+    image: "/images/mdh.jpg", // Direct path to public/images/mdh.jpg
   },
 ];
 
